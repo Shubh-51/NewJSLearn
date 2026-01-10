69d28406e83f4054dfdcd9bb8103ebff0213af9d
Shubh-51 <shubhmaurya73536@gmail.com>
2026-01-10 17:58:58 +0530

    datatypes

---
+++ 02_DataTypes/dataTypes.js
@@ -0,0 +1,31 @@
+"use Strict"; //treat all code as newer version
+
+//alert(3+3)//we are using nodejs not browser
+
+console.log(3 +
+      +  3
+)
+
+console.log("MeriSwat"); console.log(3+3) //here semi colon is necessary
+
+let name="ShubhSwat"
+let age=30
+let isLoggedIn=true
+let state=null
+
+
+//Number  =>range 2 to power 53
+// bigInt  =>
+//String =>""
+//boolean =>true/false
+
+//null => standalone value
+//undefined => no value is aasigned now
+
+//symbol => unique
+
+//object
+
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
