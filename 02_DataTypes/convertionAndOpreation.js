34c43556155270b9216759119e56ce63e46f3ec9
Shubh-51 <shubhmaurya73536@gmail.com>
2026-01-11 14:59:45 +0530

    coverstion

---
+++ 02_DataTypes/convertionAndOpreation.js
@@ -0,0 +1,66 @@
+/*let score=33
+
+console.log(typeof score)
+console.log(typeof(score))
+
+let score1="33"
+
+console.log(typeof score1)
+console.log(typeof(score1))
+
+let ValueInNumber=Number(score1)
+
+console.log(typeof ValueInNumber)
+console.log(typeof(ValueInNumber))
+
+
+let score2="33abc"
+
+let ValueInNumber1=Number(score2)
+
+console.log(typeof ValueInNumber1)
+console.log(typeof(ValueInNumber1))
+console.log(ValueInNumber1)
+
+
+let score3=null
+console.log(typeof score3)
+console.log(typeof(score3))
+
+let ValueInNumber2=Number(score3)
+
+console.log(typeof ValueInNumber2)
+console.log(typeof(ValueInNumber2))
+console.log(ValueInNumber2)
+
+let score4=undefined
+console.log(typeof score4)
+console.log(typeof(score4))
+
+let ValueInNumber3=Number(score4)
+
+console.log(typeof ValueInNumber3)
+console.log(typeof(ValueInNumber3))
+console.log(ValueInNumber3)
+
+
+let score5="Shubh"
+console.log(typeof score5)
+console.log(typeof(score5))
+
+let ValueInNumber4=Number(score5)
+
+console.log(typeof ValueInNumber4)
+console.log(typeof(ValueInNumber4))
+console.log(ValueInNumber4)
+*/
+
+let score6=true
+console.log(typeof score6)
+console.log(typeof(score6))
+
+let ValueInNumber5=Number(score6)
+
+console.log(typeof ValueInNumber5)
+console.log(typeof(ValueInNumber5))
+console.log(ValueInNumber5)
---
+++ 02_DataTypes/convertionAndOpreationBoolean.js
@@ -0,0 +1,31 @@
+let isLoggedIn=1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+let isLoggedIn1="Shubh"
+
+let booleanIsLoggedIn1=Boolean(isLoggedIn1)
+console.log(booleanIsLoggedIn1)
+
+let isLoggedIn2=""
+
+let booleanIsLoggedIn2=Boolean(isLoggedIn2)
+console.log(booleanIsLoggedIn2)
+
+let isLoggedIn3=33
+
+let booleanIsLoggedIn3=Boolean(isLoggedIn3)
+console.log(booleanIsLoggedIn3)
+
+// 1=>true ,0=>false
+//""=>false
+//"shubh"=>true
+
+
+let someNumber=33
+console.log(typeof(someNumber))
+let stringSomeNumber=String(someNumber)
+console.log(stringSomeNumber)
+console.log(typeof(stringSomeNumber))
